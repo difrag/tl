011cfaaf6fbba15bf7105c484fda394e3ba38857
Dimitris Fragkoulis <p15frag@ionio.gr>
2024-04-02 19:19:47 +0300

    Update streamlit_app.py
    tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ if uploaded_file is not None:
     st.write(data.head(10))
 
 
+
 # Add a title to the sidebar
 st.sidebar.title("Sidebar Title")
 
@@ -27,3 +28,18 @@ sidebar_input = st.sidebar.text_input("Enter some text in the sidebar:")
 # Display the sidebar input in the main area
 st.write(f"Your sidebar input: {sidebar_input}")
 
+
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
